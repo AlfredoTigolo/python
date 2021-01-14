10a9b73b934e03245b680eb186351d6dbcdb4cb5
AlfredoTigolo <alfredo.tigolo@yahoo.com>
2021-01-14 00:08:35 -0500

    adding numbers.py

---
+++ tutorial/numbers.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3
+#source https://w3schools.com/pythong/python_numbers.asp
+import random
+
+
+for x in range (10):
+    print ( random.randrange(1,10))
+else:
+    print("End Loop")
