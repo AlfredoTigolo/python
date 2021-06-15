491ddc3249b9c9239c14a35a94ee2d5df9b99294
AlfredoTigolo <45888193+AlfredoTigolo@users.noreply.github.com>
2021-06-15 09:47:11 -0700

    resolve conflict and added notes in hello.py

--- Summer2021/week1/hello.py
+++ Summer2021/week1/hello.py
@@ -3,3 +3,49 @@ if (False):
     print ("great")
 else:
     print("keep practicing")
+
+# I am typing this live
+for i in range (10):
+    print ( i )
+
+# testing gui will only work in local python intrepretor
+from tkinter import *
+
+root = Tk()
+root.mainloop()
+
+# Spring 2021 
+# https://github.com/AlfredoTigolo/python/blob/master/exam_review.txt
+
+''' adding comments with three quotes
+lecture 1 Intro to Python
+installing python 3.9 https://python.org
+python idle editor
+microsoft visual studio code https://code.visualstudio.com/
+notepad++
+
+#how do I get python intrepretor running on CMD?
+Are you on windows or macOs?
+press open window
+type cmd, enter command prompt, type python
+or
+search python
+
+python prompt >>>
+Python values, expressions and statements
+example
+>>> x = 5
+>>> 'a'
+>>> ( 5 > 3)
+>>> print ( 5 + 3)
+
+keyboard input
+x = input ("Enter something")
+
+Python Scripting
+Python Modules
+Python Library
+Solving a display issue
+Comment in Python
+Getting online help
+'''
