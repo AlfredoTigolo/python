bbbc60f0b292ac306343822963e56afed74910f4
AlfredoTigolo <45888193+AlfredoTigolo@users.noreply.github.com>
2021-06-29 09:39:25 -0700

    horoscope proejct added horoscope folder and note

---
+++ Summer2021/week2/horoscope_project/horoscope_project_notes.py
@@ -0,0 +1,40 @@
+# 6-29-2021 Summer 2021 python
+# Horoscope Project
+# focus on collecting month and day
+'''
+lecture 1 Intro to Python
+installing python 3.9 https://python.org
+python idle editor
+microsoft visual studio code https://code.visualstudio.com/
+
+python prompt >>>
+Python values, expressions and statements
+example
+>>> x = 5
+>>> 'a'
+>>> ( 5 > 3)
+>>> print ( 5 + 3)
+
+keyboard input
+x = input ("Enter something")
+
+Python Scripting
+Python Modules
+Python Library
+Solving a display issue
+Comment in Python
+Getting online help
+
+lecture 2 Data types and variables
+lecture 3 Python Operators
+lecture 4 Conditional structure if else switch case
+lecture 5 Repetition Structure for loop while loop
+'''
+
+# page 4, 5, 6 lecture 6 taking user input
+# page 8, 9, 10 lecutre 6 tradition and adjusted horoscope
+# page 10 lecture 6 convert to GUI applications
+
+# What is system design model?
+
+
