8914f692caa8ed3108a0bf2a312b42256bd1c684
AlfredoTigolo <45888193+AlfredoTigolo@users.noreply.github.com>
2022-02-03 19:18:29 -0800

    Create lab1.py
    lab 1 from spring 2022

---
+++ Spring2022/lab1.py
@@ -0,0 +1,18 @@
+def four_plus_four():
+    print(4+4)
+
+def four_times_four():
+    print(4*4)
+
+def do_math():
+    print("do_math")
+    four_times_four()
+    four_plus_four()
+
+def main():
+    print("hi")
+    do_math()
+    #print(False)
+    pass
+
+main()
