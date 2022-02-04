b8ccf6c225ce6acfff8f4cc046eff7959031f99f
AlfredoTigolo <45888193+AlfredoTigolo@users.noreply.github.com>
2022-02-03 21:57:44 -0800

    Update vagrant_macos.py
    added windows instructions on installing laravel / homestead

--- tutorial/vagrant_macos.py
+++ tutorial/vagrant_macos.py
@@ -8,3 +8,17 @@ init.sh #comment out ssh keys line in Homestead.yaml
 vagrant up #starting VM
 vagrant ssh #remoting into command line
 vagrant halt #stopping VM
+
+# for windows
+'''
+Just install "git for windows" to obtain the command line option of git
+homebrew is optional in windows
+install vagrant
+intall virtualbox
+Vagrant box add Laravel/homestead
+Git clone https://github.com/laravel/homestead.git Homestead
+init.bat #comment out ssh keys line in Homestead.yaml
+vagrant up #starting VM
+vagrant ssh #remoting into command line
+vagrant halt #stopping VM
+'''
