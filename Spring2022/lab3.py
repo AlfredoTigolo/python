c62cd6fa84f491f4dadb9e369623e131efcd7db6
AlfredoTigolo <45888193+AlfredoTigolo@users.noreply.github.com>
2022-02-10 16:58:32 -0800

    Create lab3.py

---
+++ Spring2022/lab3.py
@@ -0,0 +1,17 @@
+x = input("Please Enter First Digit ")
+y = input("Please Enter Second Digit ")
+
+if ( x.isdigit() == True and y.isdigit() == True ):
+    print ("You entered ", x )
+    print ("You entered ", y )
+else:
+    print ("You did not enter a number")
+    exit()
+
+choice = input("Would you like to add enter 0 or multiply enter 1 ?")
+if ( choice == "0" ):
+    print ( "Results ", int(x) + int(y) )
+elif ( choice == "1" ):
+    print ( "Results ", int(x) * int(y) )
+else:
+    print ("You did not enter a 0 or 1")
