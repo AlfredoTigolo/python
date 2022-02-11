dfde08766bcf08c4a54d390cd94150dbe14a848d
AlfredoTigolo <45888193+AlfredoTigolo@users.noreply.github.com>
2022-02-10 16:59:23 -0800

    Update lab3.py
    string.isdigit() checks a string has digit

--- Spring2022/lab3.py
+++ Spring2022/lab3.py
@@ -1,6 +1,7 @@
 x = input("Please Enter First Digit ")
 y = input("Please Enter Second Digit ")
 
+# string.isdigit()
 if ( x.isdigit() == True and y.isdigit() == True ):
     print ("You entered ", x )
     print ("You entered ", y )
